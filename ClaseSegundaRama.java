0335d7245647c29466f2097b4897bd3b4d0570d9
RojasAdlf <161874687+RojasAdlf@users.noreply.github.com>
2025-01-28 12:42:53 -0300

    Update ClaseSegundaRama.java

--- ClaseSegundaRama.java
+++ ClaseSegundaRama.java
@@ -1,31 +1,5 @@
 public class ClaseSegundaRama {
     public static void main(String[] args) {
-    
-        System.out.println("En esta clase programará el programador 2");
-        
-        auto miAuto = new auto();
-
-        miAuto.devolverValores();
-
-        
-
-
+        System.out.println("Este archivo fue editado desede github");
     }
-
-    
-}
-
- class auto{
-
-    byte ruedas = 4;
-    byte puertas = 2;
-    String color = "rojo";
-    String clase = "SUV";
-
-    public String devolverValores(){
-
-        return "El auto es un " + clase + ", tiene " + puertas + " puertas y es "
-        + "de color " + color;
-
-      }
 }
