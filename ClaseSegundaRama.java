408906f0f309ce343b7a71ddee516944b5c0a4ad
Mishi Rojas <soyfito12@gmail.com>
2025-01-28 11:39:03 -0300

    Editado en segundaRama

--- ClaseSegundaRama.java
+++ ClaseSegundaRama.java
@@ -2,6 +2,30 @@ public class ClaseSegundaRama {
     public static void main(String[] args) {
     
         System.out.println("En esta clase programará el programador 2");
+        
+        auto miAuto = new auto();
+
+        miAuto.devolverValores();
+
+        
+
 
     }
+
+    
+}
+
+ class auto{
+
+    byte ruedas = 4;
+    byte puertas = 2;
+    String color = "rojo";
+    String clase = "SUV";
+
+    public String devolverValores(){
+
+        return "El auto es un " + clase + ", tiene " + puertas + " puertas y es "
+        + "de color " + color;
+
+      }
 }
