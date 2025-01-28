f2c964b691250b5095600886d24bb63e4891da33
RojasAdlf <161874687+RojasAdlf@users.noreply.github.com>
2025-01-28 19:49:15 -0300

    Create multiplicacionDeDosNum.java

---
+++ multiplicacionDeDosNum.java
@@ -0,0 +1,16 @@
+public class multiplicacionDeDosNum{
+
+  public static void main(String args[]){
+
+    multiplicacionDeDosNum miMultiplicacion = new multiplicacionDeDosNum();
+
+    System.out.println(miMultiplicacion.multiplicacion(55, 12);
+
+  }
+  public int multiplicacion(int numero1, int numero2){
+
+    int resultado = numero1*numero2;
+
+    return resultado;
+    
+}
