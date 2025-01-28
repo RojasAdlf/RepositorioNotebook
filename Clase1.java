60f0bd9d4fb6dee38f70cb9c26633be57dacc48d
Mishi Rojas <soyfito12@gmail.com>
2025-01-28 11:08:04 -0300

    commit en la segunda rama

--- Clase1.java
+++ Clase1.java
@@ -4,6 +4,28 @@ public class Clase1{
     
         System.out.println("Primer commit   :)");
 
+        System.out.println("Editado desde segunda rama");
+
+        Clase1 miOperacion = new Clase1();
+
+        miOperacion.suma(55,  125);
+
+    }
+
+    public int suma(int numero1, int numero2){
+
+        int resultado = numero1+numero2;
+
+        return resultado;
+
+    }
+
+    public int multiplicaion(int numero1, int numero2){
+
+        int resultado = numero1*numero2;
+
+        return resultado;
+
     }
 
 }
---
+++ ClaseSegundaRama.java
@@ -0,0 +1,7 @@
+public class ClaseSegundaRama {
+    public static void main(String[] args) {
+    
+        System.out.println("En esta clase programará el programador 2");
+
+    }
+}
