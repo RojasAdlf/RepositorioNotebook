065d5e031543d439aceab50f60c941138ba55309
Mishi Rojas <soyfito12@gmail.com>
2025-01-28 12:48:54 -0300

    Editamos ClaseSegundaRama

--- ClaseSegundaRama.java
+++ ClaseSegundaRama.java
@@ -1,5 +1,10 @@
 public class ClaseSegundaRama {
     public static void main(String[] args) {
+
         System.out.println("Este archivo fue editado desede github");
+
+        System.out.println("Ultima actualizacion");
+
     }
+
 }
